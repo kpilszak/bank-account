d6390f4677e99fe9b016a8c5ac8f375bbbd9aa76
kpilszak <klaudia.pilszak@gmail.com>
2020-03-23 07:10:55 +0100

    Implement Checking Account Debit Card Details

--- src/Checking.java
+++ src/Checking.java
@@ -5,11 +5,18 @@ public class Checking extends Account {
     public Checking(String name, String sSN, double initDeposit) {
         super(name, sSN, initDeposit);
         accountNumber = "2" + accountNumber;
+        setDebitCard();
     }
 
+    private void setDebitCard() {
+        debitCardNumber = (int) (Math.random() * Math.pow(10, 12));
+        debitCardPIN = (int) (Math.random() * Math.pow(10, 4));
+    }
 
     public void showInfo() {
         super.showInfo();
-        System.out.println("ACCOUNT TYPE: Checking");
+        System.out.println("Your Checking Account features" +
+                "\n Debit Card Number: " + debitCardNumber +
+                "\n Debit Card PIN: " + debitCardPIN);
     }
 }
