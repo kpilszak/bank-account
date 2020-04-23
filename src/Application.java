f04246e6bb23642fa3e71b0a32aac37ab7467183
kpilszak <klaudia.pilszak@gmail.com>
2020-04-23 06:57:16 +0200

    feat: Create a list of accounts

--- src/Application.java
+++ src/Application.java
@@ -1,7 +1,10 @@
+import java.util.LinkedList;
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
+
+        List<Account> accounts = new LinkedList<Account>();
 
         String file = "src/original.csv";
 
@@ -11,15 +14,21 @@ public class Application {
             String sSN = accountHolder[1];
             String accountType = accountHolder[2];
             double initDeposit = Double.parseDouble(accountHolder[3]);
-//            System.out.println(name + " " + sSN + " " + accountType + " $" + initDeposit);
 
             if (accountType.equals("Savings")) {
-                System.out.println("OPEN  A SAVINGS ACCOUNT");
+                System.out.println("OPEN A SAVINGS ACCOUNT");
+                accounts.add(new Savings(name, sSN, initDeposit));
             } else if (accountType.equals("Checking")) {
                 System.out.println("OPEN A CHECKING ACCOUNT");
+                accounts.add(new Checking(name, sSN, initDeposit));
             } else {
                 System.out.println("ERROR READING ACCOUNT TYPE");
             }
+        }
+
+        for (Account acc : accounts) {
+            System.out.println("\n*****************************");
+            acc.showInfo();
         }
     }
 }
